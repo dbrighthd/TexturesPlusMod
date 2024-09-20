3f1a18ee62ca74432825080fb2c2a5fe9535585a
Not Flamgop <76978462+Flopgop@users.noreply.github.com>
2024-09-19 19:51:19 -0700

    Make fully asynchronous, also add a loading spinner for when the packs are downloading.

--- src/main/java/com/dbrighthd/texturesplusmod/TexturesPlusMod.java
+++ src/main/java/com/dbrighthd/texturesplusmod/TexturesPlusMod.java
@@ -2,8 +2,9 @@ package com.dbrighthd.texturesplusmod;
 
 import net.fabricmc.api.ModInitializer;
 
-public class Texturesplusmod implements ModInitializer {
+public class TexturesPlusMod implements ModInitializer {
     public static final String MODID = "texturesplusmod";
+
     @Override
     public void onInitialize() {
 
